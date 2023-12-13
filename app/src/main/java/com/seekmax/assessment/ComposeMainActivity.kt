d21146ad6b4eb5e88105a896a45e5a72343843e9
saiful islam <saifulislamtfp@gmail.com>
2023-12-13 22:12:46 +0800

    Initial project setup done

---
+++ app/src/main/java/com/seekmax/assessment/ComposeMainActivity.kt
@@ -0,0 +1,65 @@
+package com.seekmax.assessment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.seekmax.assessment.ui.screen.HomeScreen
+import com.seekmax.assessment.ui.screen.HomeScreenViewModel
+import com.seekmax.assessment.ui.screen.UserDetailScreen
+import com.seekmax.assessment.ui.screen.UserDetailScreenViewModel
+import com.seekmax.assessment.ui.theme.Practice_jetpack_composeTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ComposeMainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Practice_jetpack_composeTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val navController = rememberNavController()
+                    NavHost(navController = navController, startDestination = "home") {
+                        composable("home") {
+
+                            HomeScreen(navController)
+                        }
+                        composable("users/{userId}") { backStackEntry ->
+                            UserDetailScreen(
+                                navController,
+                                (backStackEntry.arguments?.getString("userId", "")
+                                    ?: "").toLong()
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Practice_jetpack_composeTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/main/java/com/seekmax/assessment/ui/screen/HomeScreen.kt
@@ -0,0 +1,62 @@
+package com.seekmax.assessment.ui.screen
+
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.ViewModel
+import androidx.navigation.NavController
+import com.seekmax.assessment.ComposeMainActivity
+import com.seekmax.assessment.repository.User
+import com.seekmax.assessment.repository.UserRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(navController: NavController) {
+
+    val vm: HomeScreenViewModel = hiltViewModel()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Users")
+                }
+            )
+        },
+        content = { padding ->
+            Column(modifier = Modifier.padding(padding)) {
+                val users by vm.users.collectAsState()
+                users.forEach { user ->
+                    ClickableText(text = AnnotatedString(user.name), Modifier.padding(all = 16.dp),
+                        onClick = {
+                            //    (navController.context as ComposeMainActivity).udVM.refresh()
+                            navController.navigate("users/${user.id}")
+                        })
+                }
+            }
+        }
+    )
+}
+
+@HiltViewModel
+class HomeScreenViewModel @Inject constructor(private val userRepository: UserRepository) :
+    ViewModel() {
+    private val _users = MutableStateFlow(userRepository.getUsers())
+    val users: StateFlow<List<User>> = _users
+}
